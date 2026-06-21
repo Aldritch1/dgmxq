f3ee47908b22b272baf5d9b01d71a22a8baf5ae7
zhoumoyu <1271576898@qq.com>
2026-06-21 09:29:11 +0800

    chore: remove legacy user management codebase

--- functions/[[default]].js
+++
@@ -1,8 +0,0 @@
-import { createRequestHandler } from '../src/app.js';
-
-let handler;
-
-export async function onRequest(context) {
-  handler ??= createRequestHandler({ env: context.env ?? {} });
-  return handler(context.request);
-}
--- scripts/build.js
+++
@@ -1,8 +0,0 @@
-import { access } from 'node:fs/promises';
-
-await Promise.all([
-  access('functions/[[default]].js'),
-  access('public/style.css'),
-]);
-
-console.log('EdgeOne Pages build artifacts are ready.');
