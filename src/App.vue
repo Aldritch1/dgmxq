8070f4ccdb895fd1ddecb1e72ab8f8094de0c09d
zhoumoyu <1271576898@qq.com>
2026-06-21 10:12:17 +0800

    feat: compose App.vue with all sections

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,25 @@
 <script setup>
+import Navbar from './components/Navbar.vue'
+import Hero from './components/Hero.vue'
+import ProjectGrid from './components/ProjectGrid.vue'
+import Footer from './components/Footer.vue'
+import { profile, projects } from './data/profile.js'
 </script>
 
 <template>
-  <div class="app">
-    <h1>Hello Vue</h1>
-  </div>
+  <Navbar :name="profile.name" />
+  <Hero
+    :name="profile.name"
+    :title="profile.title"
+    :bio="profile.bio"
+    :avatar="profile.avatar"
+  />
+  <ProjectGrid :projects="projects" />
+  <Footer :social="profile.social" />
 </template>
+
+<style scoped>
+.app {
+  min-height: 100vh;
+}
+</style>
